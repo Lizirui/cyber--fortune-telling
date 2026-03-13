47a1d0076a6bd8a3d894d40124f0d9f1aaae3c9c
lizirui <ruirui1990903@qq.com>
2026-03-13 20:49:28 +0800

    test: add signature verification tests

--- contracts/test/CyberFortuneNFT.t.sol
+++ contracts/test/CyberFortuneNFT.t.sol
@@ -30,4 +30,22 @@ contract CyberFortuneNFTTest is Test {
 
         assertEq(nft.ownerOf(0), user1);
     }
+
+    function test_MintWithSignature() public {
+        // 签名验证测试
+        vm.deal(user1, 1 ether);
+        vm.prank(user1);
+
+        string memory blessing = "Blessing";
+        uint8 rarity = 3;
+        uint256 expiresAt = block.timestamp + 3600;
+        uint256 nonce = 0;
+
+        bytes32 hash = keccak256(abi.encodePacked(blessing, rarity, expiresAt, nonce, user1));
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(uint256(uint160(owner)), hash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+
+        nft.mintWithSignature{value: 0.01 ether}(blessing, rarity, expiresAt, signature);
+        assertEq(nft.ownerOf(0), user1);
+    }
 }
