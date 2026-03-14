b15cb10a315fd8e418481fc07a919633ca82da0e
lizirui <ruirui1990903@qq.com>
2026-03-15 03:54:54 +0800

    chore: remove dist from git tracking

--- backend/dist/services/ai.d.ts
+++
@@ -1,6 +0,0 @@
-type Rarity = 0 | 1 | 2 | 3 | 4 | 5;
-export declare function generateBlessing(): Promise<{
-    blessing: string;
-    rarity: Rarity;
-}>;
-export {};
--- backend/dist/utils/contract.d.ts
+++
@@ -1,7 +0,0 @@
-import { ethers } from 'ethers';
-export declare const nftContract: ethers.Contract;
-export declare function getNFTInfo(tokenId: number): Promise<{
-    owner: any;
-    blessing: any;
-    rarity: number;
-}>;
